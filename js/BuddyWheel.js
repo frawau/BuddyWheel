4f162ae572cc8473e8f1a35853bd6057ffa396cc
Francois Wautier <fw@fwconsult.com>
2016-06-01 21:21:34 +0700

    Understood touch event. It is now working on tablets (Galaxy Tab A at least). The handle might need to be made bigger on touch capable devices

--- js/BuddyWheel.js
+++ js/BuddyWheel.js
@@ -88,8 +88,13 @@ colourwheel = function(target, size, prefix){
         e.preventDefault();
         var svgobj =  document.getElementById(idprefix+"colourwheel");
         var pt = svgobj.createSVGPoint();
-        pt.x = e.clientX;
-        pt.y = e.clientY;
+        if ((e.clientX)&(e.clientY)) {
+            pt.x = e.clientX;
+            pt.y = e.clientY;
+        } else  if (e.targetTouches) {
+            pt.x = e.targetTouches[0].clientX;
+            pt.y = e.targetTouches[0].clientY;
+        }
         var globalPoint = pt.matrixTransform(svgobj.getScreenCTM().inverse());
         var dragobj =  document.getElementById(idprefix+"hue-wheel");
         var globalToLocal = dragobj.getTransformToElement(svgobj).inverse();
@@ -118,16 +123,22 @@ colourwheel = function(target, size, prefix){
             myelt.setAttribute("stroke","hsl("+(Math.round(angle)+180) % 360+",80%,50%)");
     };
     function startTrackHue(e) {
-        document.addEventListener("mousemove", trackHue);
-        document.addEventListener("touchmove", trackHue);
-        document.addEventListener("mouseup", stopTrackHue);
-        document.addEventListener("touchend", stopTrackHue);
+        if ( e.type == "mousedown") {
+            document.addEventListener("mousemove", trackHue);
+            document.addEventListener("mouseup", stopTrackHue);
+        } else {
+            document.addEventListener("touchmove", trackHue);
+            document.addEventListener("touchend", stopTrackHue);
+        }
     };
     function stopTrackHue(e) {
-        document.removeEventListener("mousemove", trackHue);
-        document.removeEventListener("touchmove", trackHue);
-        document.removeEventListener("mouseup", stopTrackHue);
-        document.removeEventListener("touchend", stopTrackHue);
+        if ( e.type == "mouseup") {
+            document.removeEventListener("mousemove", trackHue);
+            document.removeEventListener("mouseup", stopTrackHue);
+        } else {
+            document.removeEventListener("touchmove", trackHue);
+            document.removeEventListener("touchend", stopTrackHue);
+        }
     };
     
     function in_SVW (px,py) {
@@ -161,8 +172,13 @@ colourwheel = function(target, size, prefix){
         e.preventDefault();
         var svgobj =  document.getElementById(idprefix+"colourwheel");
         var pt = svgobj.createSVGPoint();
-        pt.x = e.clientX;
-        pt.y = e.clientY;
+        if ((e.clientX)&(e.clientY)) {
+            pt.x = e.clientX;
+            pt.y = e.clientY;
+        } else  if (e.targetTouches) {
+            pt.x = e.targetTouches[0].clientX;
+            pt.y = e.targetTouches[0].clientY;
+        }
         var globalPoint = pt.matrixTransform(svgobj.getScreenCTM().inverse());
         var dragobj = document.getElementById(idprefix+"cw-sv-handle")
         var globalToLocal = dragobj.getTransformToElement(svgobj).inverse();
@@ -191,12 +207,22 @@ colourwheel = function(target, size, prefix){
     }
     
     function startTrackSV(e) {
-        document.addEventListener("mousemove", trackSV);
-        document.addEventListener("mouseup", stopTrackSV);
+        if ( e.type == "mousedown") {
+            document.addEventListener("mousemove", trackSV);
+            document.addEventListener("mouseup", stopTrackSV);
+        } else {
+            document.addEventListener("touchmove", trackSV);
+            document.addEventListener("touchend", stopTrackSV);
+        }
     };
     function stopTrackSV(e) {
-        document.removeEventListener("mousemove", trackSV);
-        document.removeEventListener("mouseup", stopTrackSV);
+        if ( e.type == "mouseup") {
+            document.removeEventListener("mousemove", trackSV);
+            document.removeEventListener("mouseup", stopTrackSV);
+        } else {
+            document.removeEventListener("touchmove", trackSV);
+            document.removeEventListener("touchend", stopTrackSV);
+        }
     };
 
     function create ( target, size) {
@@ -332,7 +358,7 @@ colourwheel = function(target, size, prefix){
         huehandle.addEventListener("touchstart", startTrackHue);
         svhandle.addEventListener("mousedown", startTrackSV);
         svhandle.addEventListener("touchstart", startTrackSV);
-	svElement.addEventListener("click", trackSV);
+        svElement.addEventListener("click", trackSV);
         svElement.addEventListener("touchstart", trackSV);
         hwElement.addEventListener("click", trackHue);
         hwElement.addEventListener("touchstart", trackHue);
